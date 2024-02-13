d429957ca21659ec0de9c2cc43d5c135996366cd
Margaret Alade <alade.margaret@yandex.ru>
2024-02-13 06:55:14 +0300

    Initial Commit

---
+++ Task-08.02 STL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task-08.02 STL
+//
+//  Created by Маргарет  on 13.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
